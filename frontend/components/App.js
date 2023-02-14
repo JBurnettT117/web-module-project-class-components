30f445e7e49e94a55e02719f940ed2824e9292f3
Josh Burnett <joshuaburnett429@gmail.com>
2023-02-14 14:12:22 -0600

    completed some functionality

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,14 +1,36 @@
 import React from 'react'
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          task: "Organize Garage",
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          task: "Bake Cookies",
+          id:1528817084358,
+          completed: false
+        }
+      ]
+    }
+  }
   render() {
+    const { todos } = this.state;
+    console.log(todos);
     return (
       <div>
         <h1>Todos</h1>
         <ul>
-          <li>Walk the dog</li>
-          <li>Take out trash</li>
-          <li>Workout</li>
+          {
+            todos.map(todo => {
+              return (<li>{todo.task} { todo.completed?<span>- completed</span>:<span></span> }</li>)
+            })
+          }
+          
         </ul>
 
         <form>
